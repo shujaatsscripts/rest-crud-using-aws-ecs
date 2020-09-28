fa1c8e878ee09eab211686cb3b2f37d2bddb147f
Usama Shujaat <48332443+usamashujaat1812@users.noreply.github.com>
2020-09-28 10:10:56 +0500

    task_family variable

--- service-update.sh
+++ service-update.sh
@@ -6,9 +6,9 @@ IMAGE=935648617855.dkr.ecr.us-east-2.amazonaws.com/rest-crud-using-aws-ecs:${GIT
 
 sed -e "s;IMAGE_VARIABLE;${IMAGE};g" rest-crud-task.json > rest-crud-task_${GIT_COMMIT}.json
 
-aws ecs register-task-definition --family TASK_FAMILY --cli-input-json file://rest-crud-task_${GIT_COMMIT}.json
+aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://rest-crud-task_${GIT_COMMIT}.json
 
-TASK_REVISION=`aws ecs describe-task-definition --task-definition rest-crud-task_${GIT_COMMIT} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition rest-crud-task | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 #DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 
 aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} #--desired-count ${DESIRED_COUNT}
